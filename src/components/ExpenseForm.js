69259b74e57fe8af018b6565a0fdba4aa004ff07
aschhabra <chhabra.asingh@gmail.com>
2018-08-09 21:09:48 +0000

    change button text

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -86,7 +86,7 @@ class ExpenseForm extends React.Component{
       
           </textarea>
           <div>
-          <button className="button">Add Expense</button>
+          <button className="button">Save Expense</button>
           </div>
         </form>
     );
